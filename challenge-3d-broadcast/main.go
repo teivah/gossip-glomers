a760935682914b14738bfc5e9be09d890d6ea774
Teiva Harsanyi <teiva.harsanyi@docker.com>
2023-02-25 15:57:21 +0100

    3d

---
+++ challenge-3d-broadcast/main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"sync"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	br := newBroadcaster(n, 10)
+	defer br.close()
+	s := &server{n: n, nodeID: n.ID(), ids: make(map[int]struct{}), br: br}
+
+	n.Handle("broadcast", s.broadcastHandler)
+	n.Handle("read", s.readHandler)
+	n.Handle("topology", s.topologyHandler)
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type server struct {
+	n      *maelstrom.Node
+	nodeID string
+	br     *broadcaster
+
+	idsMu sync.RWMutex
+	ids   map[int]struct{}
+}
+
+func (s *server) broadcastHandler(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	id := int(body["message"].(float64))
+	s.idsMu.Lock()
+	if _, exists := s.ids[id]; exists {
+		s.idsMu.Unlock()
+		return nil
+	}
+	s.ids[id] = struct{}{}
+	s.idsMu.Unlock()
+
+	if err := s.broadcast(msg.Src, body); err != nil {
+		return err
+	}
+
+	return s.n.Reply(msg, map[string]any{
+		"type": "broadcast_ok",
+	})
+}
+
+func (s *server) broadcast(src string, body map[string]any) error {
+	for _, dst := range s.n.NodeIDs() {
+		if dst == src || dst == s.nodeID {
+			continue
+		}
+
+		s.br.broadcast(broadcastMsg{
+			dst:  dst,
+			body: body,
+		})
+	}
+	return nil
+}
+
+func (s *server) readHandler(msg maelstrom.Message) error {
+	ids := s.getAllIDs()
+
+	return s.n.Reply(msg, map[string]any{
+		"type":     "read_ok",
+		"messages": ids,
+	})
+}
+
+func (s *server) getAllIDs() []int {
+	s.idsMu.RLock()
+	ids := make([]int, 0, len(s.ids))
+	for id := range s.ids {
+
+		ids = append(ids, id)
+	}
+	s.idsMu.RUnlock()
+
+	return ids
+}
+
+func (s *server) topologyHandler(msg maelstrom.Message) error {
+	return s.n.Reply(msg, map[string]any{
+		"type": "topology_ok",
+	})
+}
+
+type broadcastMsg struct {
+	dst  string
+	body map[string]any
+}
+
+type broadcaster struct {
+	cancel context.CancelFunc
+	ch     chan broadcastMsg
+}
+
+func newBroadcaster(n *maelstrom.Node, worker int) *broadcaster {
+	ch := make(chan broadcastMsg)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	for i := 0; i < worker; i++ {
+		go func() {
+			for {
+				select {
+				case msg := <-ch:
+					for {
+						if err := n.Send(msg.dst, msg.body); err != nil {
+							continue
+						}
+						break
+					}
+				case <-ctx.Done():
+					return
+				}
+			}
+		}()
+	}
+
+	return &broadcaster{
+		ch:     ch,
+		cancel: cancel,
+	}
+}
+
+func (b *broadcaster) broadcast(msg broadcastMsg) {
+	b.ch <- msg
+}
+
+func (b *broadcaster) close() {
+	b.cancel()
+}
